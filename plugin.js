1ba2fedf89012ce7e241cbb90b78fda6913508e8
maps4all <maps4all007@yandex.com>
2017-08-08 23:21:17 +0200

    Create plugin.js

---
+++ plugin.js
@@ -0,0 +1,10 @@
+{
+  "moduleId" : "grabowCommuter/germany_RBB",
+  "type" : "Website",
+  "name" : "CA.gov",
+  "uri" : "http://www.inforadio.de/nachrichten/verkehr/index.html",
+  "startTrigger" : "Verkehrsservice",
+  "endTrigger" : "",
+  "startTag" : "article",
+  "endTag" : "article"
+}
